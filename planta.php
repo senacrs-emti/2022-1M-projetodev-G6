3d3e964bc8f97b892e4804429aa5323f701a4b58
MateusMachado01 <108525021+MateusMachado01@users.noreply.github.com>
2022-11-10 09:29:55 -0300

    projeto

---
+++ planta.php
@@ -0,0 +1,26 @@
+<?php
+include_once 'conexao.php';
+?>
+
+
+    <main id="plantas" class="row">
+        <div class="col-4">
+            <img src="./img/orquidea.webp" alt="">
+        </div>
+        <div class="col-8">
+            <h1>orquideas</h1>
+            <h3>como cuidar?</h3>
+            <p>os cuidados principais incluem agua, sol e espaço</p>
+            <h3>substrato</h3>
+        </div>
+        <div>
+            <h2>inspirações</h2>
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+    </main>
+<?php 
+include_once 'footer.php';
+?>
